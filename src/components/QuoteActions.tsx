c59499b38bec52fffe1677c26e8f6ad3de710f28
Huw Roberts <huwrroberts@gmail.com>
2023-10-16 12:17:31 +0100

    Share button funcs

--- src/components/QuoteActions.tsx
+++ src/components/QuoteActions.tsx
@@ -13,11 +13,28 @@ export default function QuoteActions(props: {
 }) {
 	const favourites = persistentStore((state) => state.favourites);
 	const favouritesIds = favourites.map((fav) => fav.id);
+
 	const handleFavouriteClick = () => {
 		if (favouritesIds.includes(props.quote.id)) {
 			return persistentStore.getState().removeFavourite(props.quote);
 		}
 		persistentStore.getState().addFavourite(props.quote);
+	};
+
+	const handleShareX = () => {
+		window.open(
+			`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+				props.quote.quote
+			)}&hashtags=stoicmemo&url=${encodeURIComponent(window.location.href)}`
+		);
+	};
+
+	const handleShareFacebook = () => {
+		window.open(
+			`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+				window.location.href
+			)}`
+		);
 	};
 
 	return (
@@ -37,11 +54,19 @@ export default function QuoteActions(props: {
 			<div className="quoteActions__divide"></div>
 			<div className="label mr-2.5">Share</div>
 			{/* == X == */}
-			<button className="p-2.5" aria-label="Share on X/Twitter">
+			<button
+				onClick={handleShareX}
+				className="p-2.5"
+				aria-label="Share on X/Twitter"
+			>
 				<XIcon className="w-[14px]" />
 			</button>
 			{/* == Facebook == */}
-			<button className="p-2.5" aria-label="Share on Facebook">
+			<button
+				onClick={handleShareFacebook}
+				className="p-2.5"
+				aria-label="Share on Facebook"
+			>
 				<FacebookIcon className="w-[7px]" />
 			</button>
 		</div>
