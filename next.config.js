7af8d596f120586a1e5e9b87dfad0e1e0386514d
Huw Roberts <huwrroberts@gmail.com>
2023-10-25 13:07:28 +0100

    Next PWA

--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ const nextConfig = {
 	},
 };
 
-module.exports = nextConfig;
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+	...nextConfig,
+});
