9d4b2d070a4ab3dcb1e6533bd005f9df9f766e1e
Huw Roberts <huwrroberts@gmail.com>
2023-10-25 13:05:35 +0100

    Update to app way of doing meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,59 @@
 import "./css/globals.css";
 import type { Metadata } from "next";
-import Head from "next/head";
 
 export const metadata: Metadata = {
 	title: "Stoic Memo",
-	description: "",
+	description:
+		"Stoic Memo offers a unique quote from Stoic philosophy each day.",
+	openGraph: {
+		type: "website",
+		url: "https://stoic-memo.vercel.app/",
+		title: "Stoic Memo",
+		description:
+			"Stoic Memo offers a unique quote from Stoic philosophy each day.",
+		images: [
+			{
+				url: "https://stoic-memo.vercel.app/share-image.png",
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		site: "https://stoic-memo.vercel.app/",
+		creator: "@huwrobertsnet",
+		title: "Stoic Memo",
+		description:
+			"Stoic Memo offers a unique quote from Stoic philosophy each day.",
+		images: [
+			{
+				url: "https://stoic-memo.vercel.app/share-image.png",
+			},
+		],
+	},
+	icons: [
+		{
+			rel: "apple-touch-icon",
+			sizes: "180x180",
+			url: "/apple-touch-icon.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			url: "/favicon-32x32.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "16x16",
+			url: "/favicon-16x16.png",
+		},
+		{
+			rel: "manifest",
+			url: "/site.webmanifest",
+		},
+	],
+	themeColor: "#ffffff",
 };
 
 export default function RootLayout({
@@ -14,56 +63,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<Head>
-				<title>Stoic Memo</title>
-				<meta
-					name="description"
-					content="Stoic Memo offers a unique quote from Stoic philosophy each day."
-				/>
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://stoic-memo.vercel.app/" />
-				<meta property="og:title" content="Stoic Memo" />
-				<meta
-					property="og:description"
-					content="Stoic Memo offers a unique quote from Stoic philosophy each day."
-				/>
-				<meta
-					property="og:image"
-					content="https://stoic-memo.vercel.app/share-image.png"
-				/>
-
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content="https://stoic-memo.vercel.app/" />
-				<meta property="twitter:title" content="Stoic Memo" />
-				<meta
-					property="twitter:description"
-					content="Stoic Memo offers a unique quote from Stoic philosophy each day."
-				/>
-				<meta
-					property="twitter:image"
-					content="https://stoic-memo.vercel.app/share-image.png"
-				/>
-				<link
-					rel="apple-touch-icon"
-					sizes="180x180"
-					href="/apple-touch-icon.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="32x32"
-					href="/favicon-32x32.png"
-				/>
-				<link
-					rel="icon"
-					type="image/png"
-					sizes="16x16"
-					href="/favicon-16x16.png"
-				/>
-				<link rel="manifest" href="/site.webmanifest" />
-				<meta name="msapplication-TileColor" content="#da532c" />
-				<meta name="theme-color" content="#ffffff" />
-			</Head>
 			<body>{children}</body>
 		</html>
 	);
